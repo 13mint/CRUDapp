ebaeca01c9545fccafea2c46ca5ac9fb211185f5
13mint <13mint07pineapple13@gmail.com>
2026-01-10 13:05:52 +0900

    user service

--- src/main/java/web/dao/UserDAO.java
+++ src/main/java/web/dao/UserDAO.java
@@ -1,4 +1,13 @@
 package web.dao;
 
-public class UserDAO {
+import web.model.User;
+
+import java.util.List;
+
+public interface UserDAO {
+    void save(User user);
+    void update(User user);
+    void delete(long id);
+    User findById(long id);
+    List<User> findAll();
 }
--- src/main/java/web/dao/UserDAOImpl.java
+++ src/main/java/web/dao/UserDAOImpl.java
@@ -1,4 +1,42 @@
 package web.dao;
 
-public class UserDAOImpl {
+import web.model.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+public class UserDAOImpl implements UserDAO {
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public void save(User user) {
+        em.persist(user);
+    }
+
+    @Override
+    public void update(User user) {
+        em.merge(user);
+    }
+
+    @Override
+    public void delete(long id) {
+        User user = findById(id);
+        if  (user != null) {
+            em.remove(user);
+        }
+    }
+
+    @Override
+    public User findById(long id) {
+        return em.find(User.class, id);
+    }
+
+    @Override
+    public List<User> findAll() {
+        return em
+                .createQuery("from User", User.class)
+                .getResultList();
+    }
 }
