a3ef69c994bc6154f416cde74eb3c4fc45a3e5a8
13mint <13mint07pineapple13@gmail.com>
2026-01-16 16:07:32 +0900

    add mapping to usercontroler

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -2,6 +2,7 @@ package web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import web.model.User;
 import web.service.UserService;
@@ -11,20 +12,22 @@ import web.service.UserService;
 public class UserController {
     private final UserService userService;
 
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-
     public String listUsers(Model model) {
         model.addAttribute("users", userService.findAll());
         return "users";
     }
 
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/addUser")
     public String addUser(Model model) {
         model.addAttribute("user", new User());
         return "addUser";
     }
 
+    @GetMapping("/saveUser")
     public String saveUser(User user) {
         if(!userService.existsById(user.getId())){
             userService.save(user);
@@ -39,6 +42,7 @@ public class UserController {
         return "redirect:/users";
     }
 
+    @GetMapping("/editUser")
     public String editUser(User user) {
         userService.update(user);
         return "redirect:/users";
