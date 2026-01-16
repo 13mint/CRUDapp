ffaf9eae4ddf84a0b46e3800bcb6e4d374962b91
13mint <13mint07pineapple13@gmail.com>
2026-01-16 16:25:24 +0900

    add bean to dao

--- src/main/java/web/service/UserServiceImpl.java
+++ src/main/java/web/service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package web.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import web.dao.UserDAO;
 import web.dao.UserDAOImpl;
 import org.springframework.stereotype.Service;
@@ -16,25 +17,31 @@ public class UserServiceImpl implements UserService {
         this.userDAO = userDAO;
     }
 
+    @Override
     public void save(User user) {
         userDAO.save(user);
     }
 
+    @Override
     public void update(User user) {
         userDAO.update(user);
     }
 
+    @Override
     public void delete(Long id) {
         userDAO.delete(id);
     }
 
+    @Override
     public User findById(Long id) {
         return userDAO.findById(id);
     }
 
+    @Override
     public List<User> findAll() {
         return userDAO.findAll();
     }
 
+    @Override
     public boolean existsById(Long id) { return userDAO.findById(id) != null; }
 }
