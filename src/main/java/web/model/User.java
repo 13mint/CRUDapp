2686130fb3756d0d940806de20a249a8e30bbdcc
13mint <13mint07pineapple13@gmail.com>
2026-01-19 16:13:02 +0900

    validation add

--- src/main/java/web/model/User.java
+++ src/main/java/web/model/User.java
@@ -35,7 +35,7 @@ public class User {
 
     @NotNull
     @Pattern(
-            regexp = "^[A-Za-z0-9+_.-]+@(.+\\.com)$",
+            regexp = "^[A-Za-z0-9+_.-@]+(\\.com)$",
             message = "Email must end with .com"
     )
     private String email;
