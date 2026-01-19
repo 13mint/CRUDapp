02efaa8bfd8c815b86eba13e14de8953f41499dc
13mint <13mint07pineapple13@gmail.com>
2026-01-19 15:55:59 +0900

    new columns add

--- src/main/java/web/model/User.java
+++ src/main/java/web/model/User.java
@@ -9,12 +9,16 @@ public class User {
 
     private String name;
     private String surname;
+    private int age;
+    private String email;
 
     public User() {    }
 
-    public User(String name, String surname) {
+    public User(String name, String surname, int age, String email) {
         this.name = name;
         this.surname = surname;
+        this.age = age;
+        this.email = email;
     }
 
     public long getId() {
@@ -40,4 +44,12 @@ public class User {
     public void setSurname(String surname) {
         this.surname = surname;
     }
+
+    public int getAge() {return age;}
+
+    public void setAge(int age) {this.age = age;}
+
+    public String getEmail() {return email;}
+
+    public void setEmail(String email) {this.email = email;}
 }
